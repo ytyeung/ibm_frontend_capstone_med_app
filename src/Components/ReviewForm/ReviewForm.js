cc6734a0e2a97f48030ded7d8275bbb0dbf7fc87
David Yeung <sdyeung@gmail.com>
2023-10-31 12:07:00 -0400

    review form

--- src/Components/ReviewForm/ReviewForm.js
+++ src/Components/ReviewForm/ReviewForm.js
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import './ReviewForm.css';
+import Popup from 'reactjs-popup';
+
 const ReviewForm = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [username, setUsername] = useState("");
   const [doctorData, setDoctorData] = useState([]);
   const [isShowReviewForm, setIsShowReviewForm] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const storedUsername = sessionStorage.getItem('email');
-    const storedDoctorData = JSON.parse(localStorage.getItem('doctorData'));
+    let storedDoctorData = JSON.parse(localStorage.getItem('doctorData'));
 
     if (storedDoctorData){
+        let i=1;
+        storedDoctorData.forEach(element => {element.id = i++;});
+
         setDoctorData(storedDoctorData);
         //const storedAppointmentData = JSON.parse(localStorage.getItem(storedDoctorData[1]?.name));
         //if (storedAppointmentData) {
@@ -28,6 +34,34 @@ const ReviewForm = ({ children }) => {
     }
   }, []);
 
+  const handleFormSubmit = (appointmentData) => {
+    const newAppointment = {
+      id: uuidv4(),
+      ...appointmentData,
+    };
+    const updatedAppointments = [...appointments, newAppointment];
+    setAppointments(updatedAppointments);
+    setShowModal(false);
+
+    const doctorData = {'name': name, 'speciality': speciality};
+    const storedDoctorData = JSON.parse(localStorage.getItem('doctorData'));
+    let updatedDoctorData = [];
+
+    if (storedDoctorData){
+        if (!storedDoctorData.includes(doctorData)){
+            updatedDoctorData = [...storedDoctorData, doctorData];
+        }
+    }else{
+        updatedDoctorData =[doctorData];
+    }
+
+    localStorage.setItem('doctorData',JSON.stringify(updatedDoctorData));
+    localStorage.setItem(name,JSON.stringify(newAppointment));
+
+    window.location.reload();
+
+  };
+
   return (
       <div className="ReviewFormPane">
         <div className="ReviewFormText">Reviews</div>
@@ -43,12 +77,25 @@ const ReviewForm = ({ children }) => {
             </tr>
         </thead>
         <tbody>
-        {doctorData.map( (doctor,i) => (
+        {doctorData.map( (doctor) => (
             <tr key={doctor?.name}>
-                <td>{i++}</td>
+                <td></td>
                 <td style={{textAlign:'center'}}>{doctor?.name}</td>
                 <td style={{textAlign:'center'}}>{doctor?.speciality}</td>
-                <td></td>
+                <td>
+                    <Popup style={{ backgroundColor: '#FFFFFF'}}
+                        trigger={
+                        <button className="ReviewButton">
+                            Click Me
+                        </button>
+                        }
+                        modal open={showModal} onClose={() => setShowModal(false)}>
+                        
+                        {(close) => ( 
+                            <AppointmentForm onSubmit={handleFormSubmit} />
+                        )}
+                    </Popup>
+                </td>
                 <td></td>
 
             </tr>)
